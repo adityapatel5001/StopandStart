ec09637d759f7eab4bee3a7ae273defb8bba4a89
adityapatel5001 <rishurocks95@gmail.com>
2026-03-13 12:34:07 -0400

    changes done in script

--- start.sh
+++ start.sh
@@ -4,13 +4,13 @@ REGION="us-east-2"
 
 INSTANCE_IDS=$(aws ec2 describe-instances \
   --region "$REGION" \
-  --filters "Name=tag:autorestart,Values=true" "Name=instance-state-name,Values=stopped\\" \
+  --filters "Name=tag:autorestart,Values=true" "Name=instance-state-name,Values=stopped" \
   --query "Reservations[*].Instances[*].InstanceId" \
   --output text)
 
 if [ -z "$INSTANCE_IDS" ]; then
   echo "No running instances found with autoschedule=true"
-else
+elses
   echo "Starting instances: $INSTANCE_IDS"
   aws ec2 stop-instances --region "$REGION" --instance-ids $INSTANCE_IDS
 fi
